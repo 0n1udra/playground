00642a69b028689389f925b742baab9ece7265aa
Drake Thomas <drake550@pm.me>
2020-09-29 02:52:58 -0400

    added some more argument features.
    Even if copying account files was successful, it'll raise an error anyway, idk what it's for, but everything works anyway....*shrug*

--- utils/discord_account_switcher.py
+++ utils/discord_account_switcher.py
@@ -9,13 +9,17 @@ def start(): os.system(f"{discord_local}/Update.exe --processStart Discord.exe")
 
 def backup():
     name = str(input("Save name > "))
-    shutil.copytree(f'{discord_roaming}/leveldb/', f'{discord_roaming}/{name}')
+    try: shutil.copytree(f'{discord_roaming}/leveldb/', f'{discord_roaming}/{name}')
+    except: pass
+    if os.path.isdir(f"{discord_roaming}/{name}"):
+        print("Added new account.")
+    else: print("Error adding account.")
 
 
 def restore():
     stop()
     print(os.listdir(discord_roaming))
-    name = str(input("Save name > "))
+    name = str(input("Name > "))
     if os.path.isdir(f'{discord_roaming}/leveldb'):
         shutil.rmtree(f"{discord_roaming}/leveldb")
     shutil.copytree(f'{discord_roaming}/{name}', f'{discord_roaming}/leveldb')
@@ -28,7 +32,17 @@ def reset():
     time.sleep(1)
     start()
 
+def remove():
+    stop()
+    print(os.listdir(discord_roaming))
+    name = str(input("Account > "))
+    if os.path.isdir(f'{discord_roaming}/{name}'):
+        shutil.rmtree(f"{discord_roaming}/{name}")
+
 if __name__ == '__main__':
-    if '-b' in sys.argv: backup()
-    if '-r' in sys.argv: restore()
-    if '-reset' in sys.argv: reset()
+    if 'new' in sys.argv or '-n' in sys.argv: backup()
+    if 'change' in sys.argv or '-c' in sys.argv: restore()
+    if 'delete' in sys.argv or '-d' in sys.argv: remove()
+    if 'reset' in sys.argv: reset()
+    if 'start' in sys.argv: start()
+    if 'stop' in sys.argv: stop()
