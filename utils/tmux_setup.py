ad5e15eb31bd282d1658ff828e158f1daedaa314
0n1udra <dt01@pm.me>
2021-12-01 21:15:50 -0500

    starts tmux session (4 windows) can start scripts or programs
    can start slime_server and channel17_bot, can also start ssh and bpytop in their own windows/panes.
    Used with new aliases in .profile, tsetup.
    tsetup doesn't do anything, just a alias to the script.
    tsetupapp for now sets up windows/pane, and starts ssh(window 2 top pane) and bpytop(window 4).
    tsetupall does what tsetupapp does but also starts the two bots and attaches to session.

---
+++ utils/tmux_setup.py
@@ -0,0 +1,35 @@
+import time, sys, os
+
+tmux_session_name = 'sess2'
+
+def start_tmux_session():
+    """Starts Tmux session in detached mode, with 2 panes, and sets name."""
+
+    # Start tmux session
+    os.system(f'tmux new -d -s {tmux_session_name}')
+
+    # === Server window
+    # Renames window, create 2 bottom panes (3 total for first window)
+    os.system(f'tmux rename-window -t {tmux_session_name} Server && tmux split-window -v -t {tmux_session_name}:1.0 && tmux split-window -h -t {tmux_session_name}:1.1')
+
+    # === SSH window
+    # New window, renames window, new vertical pane
+    os.system(f'tmux new-window -t {tmux_session_name} && tmux rename-window -t {tmux_session_name}:2 SSH && tmux split-window -v -t {tmux_session_name}:2.0')
+
+    # === bash window
+    os.system(f'tmux new-window -t {tmux_session_name} && tmux rename-window -t {tmux_session_name}:3 Stuff && tmux split-window -v -t {tmux_session_name}:3.0')
+
+    # === Monitor window
+    os.system(f'tmux new-window -t {tmux_session_name} && tmux rename-window -t {tmux_session_name}:4 btop')
+
+    time.sleep(1)
+
+if __name__ == '__main__':
+    if 'starttmux' in sys.argv: start_tmux_session()
+    if 'startbots' in sys.argv:
+        os.system(f'tmux send-keys -t {tmux_session_name}:1.1 "slimebot" ENTER')  # Start slime_server bot
+        os.system(f'tmux send-keys -t {tmux_session_name}:1.2 "sandownbot" ENTER')  # Start channel17 bot
+    if 'startapp' in sys.argv:
+        os.system(f'tmux send-keys -t {tmux_session_name}:2.0 "ssh mbp13" ENTER')  # SSH MBP13
+        os.system(f'tmux send-keys -t {tmux_session_name}:4.0 "btop" ENTER')  # Start bpytop
+    if 'attachtmux' in sys.argv: os.system(f"tmux attach -t {tmux_session_name}")
