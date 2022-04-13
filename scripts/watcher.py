c60d6a9376ec4869057a89f5fbf9823188cb8e0e
0n1udra <dt01@pm.me>
2022-04-13 00:18:08 -0400

    updated message

--- scripts/watcher.py
+++ scripts/watcher.py
@@ -1,4 +1,4 @@
-import  os
+import os
 from extra import lprint
 
 sess_name = 'sess'
@@ -6,6 +6,7 @@ ctx = 'water.py'
 
 # Checks if tmux session exists. os.system will return 0 if successful, so if anything else but zero it probably failed.
 if os.system(f'tmux ls | grep {sess_name}'):
-    lprint(ctx, "INFO: Executing tmux_setup.py")
+    lprint(ctx, "INFO: Tmux 'sess' not found. Executing tmux_setup.py")
     if os.system(f'python3 ~/git/playground/scripts/tmux_setup.py starttmux startbots startserver startapps attachtmux'):
         lprint(ctx, "ERROR: Executing tmux_setup.py")
+    else: lprint(ctx, "INFO: Finished tmux_setup.py")
