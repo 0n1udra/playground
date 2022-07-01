f35219813b5ea898cc82cc6025b859785f2c2efd
0n1udra <dt01@pm.me>
2022-07-01 19:13:44 -0400

    pycharm settings

---
+++ test.py
@@ -0,0 +1,10 @@
+context = {'user':
+                {
+                'code': {'name':'NAME', 'details': 'hi'},
+                'code2': {'name':'NAME2', 'details': 'ok'}
+                },
+               }
+
+
+for i in context:
+    print(i['user'])
