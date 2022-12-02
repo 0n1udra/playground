f50a75e805c33803f762358dca627d5f12c991e2
0n1udra <dt01@pm.me>
2022-12-01 23:48:49 -0500

    Updated watcher script
    At startup, won't start tmux session until internet connection (~4min).
    
    New feature checkbots, checks if discord bot processes are running. Sends Discord msg of offline ones.

--- scripts/watcher.py
+++ scripts/watcher.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
-import time, sys, os
+import psutil, time, sys, os
 from extra import lprint
 
 sess_name = 'sess'
 filename = 'watcher.py'
 
-# Checks if tmux session exists. os.system will return 0 if successful, so if anything else but zero it probably failed.
-if os.system(f'tmux ls | grep {sess_name}'):
-
-    lprint(filename, "INFO: Tmux 'sess' not found. Executing tmux_setup.py")
-    if os.system(f'python3 ~/git/playground/scripts/tmux_setup.py starttmux startbots'):
-        lprint(filename, "ERROR: Executing tmux_setup.py")
+# Tests for internet. Sends message to Discord channel if ping works, else will try for ~4min, 30s sleep.
+for i in range(8):
+    if os.system('ping -c 2 1.1.1.1'): time.sleep(30)
     else:
-        lprint(filename, "INFO: Finished tmux_setup.py")
+        # Checks if tmux session exists. os.system will return 0 if successful, so if anything else but zero it probably failed.
+        if os.system(f'tmux ls | grep {sess_name}'):
+            lprint(filename, "INFO: Tmux 'sess' not found. Executing tmux_setup.py")
+            if os.system(f'python3 ~/git/playground/scripts/tmux_setup.py starttmux startbots'):
+                os.system(f"python3 ~/git/playground/scripts/matsumoto.py 'Arcpy: Tmux session initiated'")
+                lprint(filename, "ERROR: Executing tmux_setup.py")
+            else: lprint(filename, "INFO: Finished tmux_setup.py")
+        break
 
-        # Tests for internet. Sends message to Discord channel if ping works, else will try for ~4min, 30s sleep.
-        for i in range(8):
-            if os.system('ping -c 2 1.1.1.1'):
-                time.sleep(30)
-            else:
-                os.system(f"python3 ~/git/playground/scripts/matsumoto.py 'Arcpy: Rebooted'")
-                lprint(filename, "INFO: Internet restored")
-                break
+def checkbots():
+    """
+    Checks if python3 bot process are running.
+    Sends Discord message of what bots are offline
+    """
+
+    discord_bots = {'slime': 'run_bot.py', 'liquor': 'liquor_bot.py', 'channel17': 'channel17_bot.py'}
+    for proc in psutil.process_iter():
+        for k, v in discord_bots.items():
+            if v in proc.cmdline() and 'watcher.py' not in proc.cmdline():
+                discord_bots[k] = ''
+
+    # Create new list from dictionary only if a value exist.
+    offline_bots = ', '.join([k for k, v in discord_bots.items() if v])
+    if offline_bots: os.system(f"python3 ~/git/playground/scripts/matsumoto.py **Bots Offline:** {offline_bots}")
 
 def check_liquor_site():
     # Checks liquor_site status, if offline sends Discord message.
@@ -36,6 +47,10 @@ def check_liquor_site():
             os.system("python3 ~/git/playground/scripts/matsumoto.py Liquor site unreachable")
             with open('lsite_status', 'w') as f: f.write('offline')
 
+
 if __name__ == '__main__':
     if 'lsite' in sys.argv:
         check_liquor_site()
+
+    if 'checkbots' in sys.argv:
+        checkbots()
