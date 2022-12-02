647789795388fd59f4f8ec1c9356439b3655a7b4
0n1udra <dt01@pm.me>
2022-12-01 23:49:28 -0500

    minor update

--- scripts/pkill.py
+++ scripts/pkill.py
@@ -7,10 +7,12 @@ def get_proc(proc_name, proc_cmdline=None):
     """Returns a process by matching name and argument."""
     procs = []
     for proc in psutil.process_iter():
-        try:
-            proc.name().lower().index(proc_name.lower())
-        except: continue
-        else: procs.append(proc)
+        if proc_name.lower() in proc.name().lower():
+            if proc_cmdline:
+                if proc_cmdline in proc.cmdline():
+                    procs.append(proc)
+                else: continue
+            else: procs.append(proc)
     return procs
 
 def kill_procs(procs):
@@ -30,8 +32,10 @@ procs += get_proc('explorer')
 if __name__ == '__main__':
 
     if 'list' in sys.argv:
+        proc_cmdline = ''
+        if len(sys.argv) == 4: proc_cmdline = sys.argv[3]
         try:
-            for i in get_proc(sys.argv[2]): print(i)
+            for i in get_proc(sys.argv[2], proc_cmdline): print(i, i.cmdline())
         except: print('Usage: pkill.py [list] proc_name')
         exit()
 
