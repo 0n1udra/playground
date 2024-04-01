1d0c2d1b365303fade16fa46a159492bac1af8b5
0n1udra <dt01@pm.me>
2024-04-01 13:46:01 -0400

    Can now use function to send discord msg

--- scripts/extra.py
+++ scripts/extra.py
@@ -1,10 +1,16 @@
-import datetime, os
+import datetime, inspect, os
 
 # Creates log file in ~/ if not exist
 log_file = f'{os.getenv("HOME")}/system.log'
 if not os.path.exists(log_file):
     file = open(log_file, 'w+')
     file.close()
+
+def dmsg(msg):
+    try: src = inspect.stack()[1].filename
+    except: src = None
+
+    os.system(f"python3 ~/git/playground/scripts/matsumoto.py '({src}) {msg}'")
 
 # For printing and logging events
 def lprint(location, text):
